9b0b3d0be525486b7d545a023828e46bee9e5186
Sergei Winitzki <winitzki@gmail.com>
2017-06-07 11:29:40 -0700

    update for 0.2.0

--- src/main/scala/sample/HelloWorldChymyst.scala
+++ src/main/scala/sample/HelloWorldChymyst.scala
@@ -6,18 +6,15 @@ object HelloWorldChymyst extends App {
   // Define a function that starts some reactions, then call that function.
 
   def startReactions(): Boolean = {
-    val tp = new FixedPool(4)
-
     val a = m[String]
     val get_status = b[Unit, Boolean]
 
-    site(tp)(
-      go { case a(message) + get_status(_, r) => println(message); r(true) }
+    site(
+      go { case a(message) + get_status(_, reply) => println(message); reply(true) }
     )
 
     a("Hello, Chymyst lab!")
     val status: Boolean = get_status()
-    tp.shutdownNow()
     status
   }
 
